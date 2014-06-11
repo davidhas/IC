df8fdaad76dac959b3beb10bb0a4c3a98cc1f70d
David Hasmüller <davidhas@gmx.de>
2014-06-11 16:37:38 +0200

    html

--- IC/settings.py
+++ IC/settings.py
@@ -2,7 +2,10 @@
 import os
 
 DEBUG = True
+
 TEMPLATE_DEBUG = DEBUG
+
+TEMPLATE_DIRS = os.path.join('','templates')
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -62,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join('','static')
+STATIC_ROOT = os.path.join('','sitestatic')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -120,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'foundation',
     'django.contrib.admin',
+    'stilwiese',
     # Uncomment the next line to enable the admin:
     # Uncomment the next line to enable admin documentation:
 )
