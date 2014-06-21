13d6cea5c62c00c1e25c0c77183281e815de0245
David Hasmüller <davidhas@gmx.de>
2014-06-22 01:02:09 +0200

    changed staticfiles_finder order

--- IC/settings.py
+++ IC/settings.py
@@ -89,8 +89,8 @@ MEDIA_URL = '/media/'
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
