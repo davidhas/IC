460e5c18a72d3b4a142a0a1b81c4d135721beb20
David Hasmüller <davidhas@gmx.de>
2014-07-24 10:22:28 +0200

    menu

--- stilwiese/urls.py
+++ stilwiese/urls.py
@@ -6,8 +6,8 @@ urlpatterns = patterns('',
 
                        # pattern: url to a view, view function, reverse name used to dynamically specify urls rather
                        # than hard coding them.
-                       url(r'^', 'stilwiese.views.index', name='home'),
-                       url(r'^stilwiese/', 'stilwiese.views.map', name='stilwiese'),
+                       url(r'^$', 'stilwiese.views.index', name='home'),
+                       url(r'^stilwiese/$', 'stilwiese.views.map', name='stilwiese'),
                        url(r'^blog/$', 'stilwiese.views.blog', name='blog'),
                        url(r'^myFashionbox/$', 'stilwiese.views.fashionbox', name='fashionbox'),
 )
